b5abf9cfcfdfbd5e3527995add7073709c3d5d19
MD Zisan <zisanislam01@gmail.com>
2023-05-03 04:08:48 +0600

    chefrecipes loaded

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app =  express();
 const port = process.env.PORT || 5000
 const chefs= require('./chef-details.json')
+const cors = require('cors')
+const chefrecipes = require('./recipes.json')
+
+app.use(cors());
 
 app.get('/',(req,res)=>{
 
 res.send('Hello World!')
-})
+});
 
 app.get('/chefs',(req,res)=>{
     res.send(chefs)
+});
+
+
+app.get('/chefrecipes/:id',(req,res)=>{
+    const id = req.params.id;
+    const chefrecipe = chefrecipes.filter(recipes=>recipes.chef_id === id)
+
+    res.send(chefrecipe)
+    
 })
+
+
 
 
 app.listen(port,()=>{
